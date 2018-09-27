bc81807f1804355ea158e3d5459e5689ee1e9346
pramudityo.pratanto <pramudityo.pratanto@ID-L1719.hcg.homecredit.net>
2018-09-27 18:00:14 +0700

    Commit date : 27-9-2018

---
+++ Test Suites/My Home Credit Mobile App.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>My Home Credit Mobile App</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-27T17:55:39</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>45c42ae6-82cb-4807-b363-c6f53a13617a</testSuiteGuid>
+   <testCaseLink>
+      <guid>553a9be3-50ac-464c-86b7-e0ce96ff9b23</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Open</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>93b48907-72b9-4eb8-8e23-318aac85e160</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a856c71e-454e-4d85-8237-bfd300712b52</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
